15672cb644012fa0931a6a3d72c9ea28b2af358d
RobertoPerezAngulo <RobertoPerezAngulo@users.noreply.github.com>
2023-08-08 15:35:20 +0000

    deploy: 19fa2f34a145cc82556c359e317051110c4172ff

--- _nuxt/static/1691298233/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortafolio\u002F_nuxt\u002Fstatic\u002F1691298233",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002FPortafolio\u002F",assetsPath:"\u002FPortafolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
